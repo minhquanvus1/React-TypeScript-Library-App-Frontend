c7acc3181747e799d208a85b0db10826c3078027
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-16 14:53:20 +0700

    create ReturnBook as Child Component of Carousel for reusability

--- src/layouts/HomePage/Carousel.tsx
+++ src/layouts/HomePage/Carousel.tsx
@@ -1,3 +1,5 @@
+import { ReturnBook } from "./ReturnBook";
+
 export const Carousel = () => {
   return (
     <div className="container mt-5" style={{ height: 550 }}>
@@ -32,40 +34,16 @@ export const Carousel = () => {
           </div>
           <div className="carousel-item">
             <div className="row d-flex justify-content-center align-items-center">
-              <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                <div className="text-center">
-                  <img
-                    src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
-                    width="151"
-                    height="233"
-                    alt="book"
-                  />
-                  <h6 className="mt-2">Book</h6>
-                  <p>Luv2Code</p>
-                  <a className="btn main-color text-white" href="#">
-                    Reserve
-                  </a>
-                </div>
-              </div>
+              <ReturnBook />
+              <ReturnBook />
+              <ReturnBook />
             </div>
           </div>
           <div className="carousel-item">
             <div className="row d-flex justify-content-center align-items-center">
-              <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                <div className="text-center">
-                  <img
-                    src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
-                    width="151"
-                    height="233"
-                    alt="book"
-                  />
-                  <h6 className="mt-2">Book</h6>
-                  <p>Luv2Code</p>
-                  <a className="btn main-color text-white" href="#">
-                    Reserve
-                  </a>
-                </div>
-              </div>
+              <ReturnBook />
+              <ReturnBook />
+              <ReturnBook />
             </div>
           </div>
         </div>
@@ -97,21 +75,9 @@ export const Carousel = () => {
       {/*Mobile*/}
       <div className="d-lg-none mt-3">
         <div className="row d-flex justify-content-center align-items-center">
-          <div className="text-center">
-            <img
-              src={require("./../../Images/BooksImages/book-luv2code-1000.png")}
-              width="151"
-              height="233"
-              alt="book"
-            />
-            <h6 className="mt-2">
-              <b>Book</b>
-            </h6>
-            <p>Luv2Code</p>
-            <a className="btn main-color text-white" href="#">
-              Reserve
-            </a>
-          </div>
+          <ReturnBook />
+          <ReturnBook />
+          <ReturnBook />
         </div>
       </div>
       <div className="homepage-carousel-title mt-3">
