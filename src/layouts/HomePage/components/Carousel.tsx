7cd69cbf75a7178458158d290c6367b8b137513b
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-16 22:31:50 +0700

    add loading Spinner for displaying Carousel

--- src/layouts/HomePage/components/Carousel.tsx
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -1,6 +1,7 @@
 import { ReturnBook } from "./ReturnBook";
 import { useEffect, useState } from "react";
 import { BookModel } from "../../../models/BookModel";
+import { SpinnerLoading } from "../../utils/SpinnerLoading";
 
 export const Carousel = () => {
   const [books, setBooks] = useState<BookModel[]>([]);
@@ -42,11 +43,7 @@ export const Carousel = () => {
   }, []);
 
   if (isLoading) {
-    return (
-      <div className="container m-5">
-        <p>Loading...</p>
-      </div>
-    );
+    return <SpinnerLoading />;
   }
 
   if (httpError) {
