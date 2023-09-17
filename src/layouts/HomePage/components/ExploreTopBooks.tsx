ef2497115178e1a181c6595714dc4220f37ed573
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-17 16:15:54 +0700

    add NavLink, and Link tag for Routing

--- src/layouts/HomePage/components/ExploreTopBooks.tsx
+++ src/layouts/HomePage/components/ExploreTopBooks.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export const ExploreTopBooks = () => {
   return (
     <>
@@ -6,13 +8,13 @@ export const ExploreTopBooks = () => {
           <div>
             <h1 className="display-5 fw-bold">Find your next adventure</h1>
             <p className="col-md-8 fs-4">Where would you like to go next?</p>
-            <a
+            <Link
               type="button"
               className="btn main-color btn-lg text-white"
-              href="#"
+              to="/search"
             >
               Explore Top Books
-            </a>
+            </Link>
           </div>
         </div>
       </div>
