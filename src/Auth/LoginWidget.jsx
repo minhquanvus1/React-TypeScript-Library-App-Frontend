3afaf2002303a81d2bcacc7f827df740ff0adaca
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-17 23:43:59 +0700

    create LoginWidget

---
+++ src/Auth/LoginWidget.jsx
@@ -0,0 +1,19 @@
+import {Redirect} from 'react-router-dom';
+import { useOktaAuth } from '@okta/okta-react';
+import { log } from 'console';
+import { SpinnerLoading } from '../layouts/utils/SpinnerLoading';
+
+const LoginWidget = ({config}) => {
+    const {oktaAuth, authState} = useOktaAuth();
+    const onSuccess = (tokens) => {
+        oktaAuth.handleLoginRedirect(tokens);
+    }
+    const onError = (err) => {
+        console.log('Sign in Error: ', err);
+    }
+    if(!authState) {
+        return (<SpinnerLoading />)
+    }
+    return authState.isAuthenticated ? <Redirect to={{pathname: "/"}}></Redirect> : <div></div>
+}
+export default LoginWidget;
