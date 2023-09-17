44077015a1bd48d18ad33e2ed0aad5bc0535c7f1
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-17 23:57:49 +0700

    create LoginWidget.jsx, and OktaLogInWidget.jsx

--- src/Auth/LoginWidget.jsx
+++ src/Auth/LoginWidget.jsx
@@ -2,6 +2,7 @@ import {Redirect} from 'react-router-dom';
 import { useOktaAuth } from '@okta/okta-react';
 import { log } from 'console';
 import { SpinnerLoading } from '../layouts/utils/SpinnerLoading';
+import OktaLogInWidget from './OktaLogInWidget';
 
 const LoginWidget = ({config}) => {
     const {oktaAuth, authState} = useOktaAuth();
@@ -14,6 +15,6 @@ const LoginWidget = ({config}) => {
     if(!authState) {
         return (<SpinnerLoading />)
     }
-    return authState.isAuthenticated ? <Redirect to={{pathname: "/"}}></Redirect> : <div></div>
+    return authState.isAuthenticated ? <Redirect to={{pathname: "/"}}></Redirect> : <OktaLogInWidget config={config} onSuccess={onSuccess} onError={onError}/>
 }
 export default LoginWidget;
