01f0b1584da0047c20aaa26180987f24010fe0ed
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-17 23:27:56 +0700

    add configuration to connect React app to Okta Authorization Server

---
+++ src/lib/oktaConfig..ts
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+    clientId: '0oabcn50qhlh7szMi5d7',
+    issuer: 'https://dev-96351608.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true
+}
