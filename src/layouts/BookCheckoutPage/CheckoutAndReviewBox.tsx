235810d7e6a94f3cd82051fcd6e8dd7ce7b8bf07
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-17 19:47:30 +0700

    add CheckoutAndReviewBox for BookCheckoutPage

---
+++ src/layouts/BookCheckoutPage/CheckoutAndReviewBox.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { BookModel } from "../../models/BookModel";
+import { Link } from "react-router-dom";
+
+export const CheckoutAndReviewBox: React.FC<{
+  book: BookModel | undefined;
+  mobile: boolean;
+}> = (props) => {
+  return (
+    <div
+      className={
+        props.mobile ? "card d-flex mt-5" : "card col-3 container d-flex mb-5"
+      }
+    >
+      <div className="card-body container">
+        <div className="mt-3">
+          <p>
+            <strong>0/5 </strong>
+            books checked out
+          </p>
+          <hr />
+          {props.book &&
+          props.book.copiesAvailable &&
+          props.book.copiesAvailable > 0 ? (
+            <h4 className="text-success">Available</h4>
+          ) : (
+            <h4 className="text-danger">Wait list</h4>
+          )}
+          <div className="row">
+            <p className="col-6 lead">
+              <strong>{props.book?.copies} </strong>
+              copies
+            </p>
+            <p className="col-6 lead">
+              <strong>{props.book?.copiesAvailable} </strong>
+              available
+            </p>
+          </div>
+        </div>
+        <Link to="/#" className="btn btn-success btn-lg">
+          Sign in
+        </Link>
+        <hr />
+        <p className="mt-3">
+          This number can change until placing order has been complete.
+        </p>
+        <p>Sign in to be able to leave a review.</p>
+      </div>
+    </div>
+  );
+};
