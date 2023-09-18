40fac564a97b3117098968a8788c29951fb84b42
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-18 08:22:13 +0700

    add Logout functionality, and Link Login button to Login Page

--- src/layouts/NavbarAndFooter/Navbar.tsx
+++ src/layouts/NavbarAndFooter/Navbar.tsx
@@ -1,6 +1,15 @@
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { useOktaAuth } from "@okta/okta-react";
+import { SpinnerLoading } from "../utils/SpinnerLoading";
 
 export const Navbar = () => {
+  const { oktaAuth, authState } = useOktaAuth();
+  if (!authState) {
+    return <SpinnerLoading />;
+  }
+  const handleLogout = async () => oktaAuth.signOut();
+  console.log(authState);
+
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-dark main-color py-3">
@@ -31,11 +40,29 @@ export const Navbar = () => {
               </li>
             </ul>
             <ul className="navbar-nav ms-auto">
-              <li className="nav-item m-1">
-                <a type="button" className="btn btn-outline-light" href="#">
-                  Sign in
-                </a>
-              </li>
+              {/* if user is NOT authenticated, then show Sign in button, else show
+              Sign out button */}
+              {!authState.isAuthenticated ? (
+                <li className="nav-item m-1">
+                  <Link
+                    type="button"
+                    className="btn btn-outline-light"
+                    to="/login"
+                  >
+                    Sign in
+                  </Link>
+                </li>
+              ) : (
+                <li>
+                  <button
+                    type="button"
+                    className="btn btn-outline-light"
+                    onClick={handleLogout}
+                  >
+                    Sign out
+                  </button>
+                </li>
+              )}
             </ul>
           </div>
         </div>
