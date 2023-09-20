3463c90d151332815c1fd769686eb78dcc277a66
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-20 14:49:44 +0700

    create MessageModel

---
+++ src/models/MessageModel.ts
@@ -0,0 +1,13 @@
+export class MessageMode {
+title: string;
+question: string;
+id?: number;
+userEmail?: string;
+adminEmail?: string;
+response?: string;
+closed?: boolean;
+    constructor(title: string, question: string) {
+        this.title = title;
+        this.question = question;
+    }
+}
