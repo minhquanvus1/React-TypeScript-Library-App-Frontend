455e158dc38dec6318c0fc1e6521277b5fa7c01a
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-20 21:23:19 +0700

    create AdminMessage component, to display a message card from non-admin user

--- src/layouts/ManageLibraryPage/components/AdminMessages.tsx
+++ src/layouts/ManageLibraryPage/components/AdminMessages.tsx
@@ -2,56 +2,84 @@ import { useOktaAuth } from "@okta/okta-react";
 import { useEffect, useState } from "react";
 import { MessageModel } from "../../../models/MessageModel";
 import { SpinnerLoading } from "../../utils/SpinnerLoading";
+import { Pagination } from "../../utils/Pagination";
+import { AdminMessage } from "./AdminMessage";
 
 export const AdminMessages = () => {
+  const { authState } = useOktaAuth();
 
-    const {authState} = useOktaAuth();
+  // normal loading states
+  const [isLoadingMessages, setIsLoadingMessages] = useState(false);
+  const [httpError, setHttpError] = useState(null);
 
-    // normal loading states
-    const [isLoadingMessages, setIsLoadingMessages] = useState(false);
-    const [httpError, setHttpError] = useState(null);
+  // messages states
+  const [messages, setMessages] = useState<MessageModel[]>([]);
+  const [messagesPerPage] = useState(5);
 
-    // messages states
-    const [messages, setMessages] = useState<MessageModel[]>([]);
-    const [messagesPerPage] = useState(5);
+  // Pagination states
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
-    // Pagination states
-    const [currentPage, setCurrentPage] = useState(1);
-    const [totalPages, setTotalPages] = useState(0);
+  useEffect(() => {
+    const fetchUserMessages = async () => {
+      if (authState && authState.isAuthenticated) {
+        const url: string = `http://localhost:8080/api/messages/search/findByClosed?closed=false&page=${
+          currentPage - 1
+        }&size=${messagesPerPage}`;
+        const messagesResponse = await fetch(url);
+        if (!messagesResponse.ok) {
+          throw new Error("Something went wrong!");
+        }
+        const messagesResponseJson = await messagesResponse.json();
+        setMessages(messagesResponseJson._embedded.messages);
+        setTotalPages(messagesResponseJson.page.totalPages);
+      }
+      setIsLoadingMessages(false);
+    };
+    fetchUserMessages().catch((error: any) => {
+      setIsLoadingMessages(false);
+      setHttpError(error.message);
+    });
+    window.scrollTo(0, 0);
+  }, [authState, currentPage]);
 
-    useEffect(() => {
-        const fetchUserMessages = async () => {
-            if(authState && authState.isAuthenticated) {
-                const url: string = `http://localhost:8080/api/messages/search/findByClosed?closed=false&page=${currentPage - 1}&size=${messagesPerPage}`;
-                const messagesResponse = await fetch(url);
-                if(!messagesResponse.ok) {
-                    throw new Error("Something went wrong!");
-                }
-                const messagesResponseJson = await messagesResponse.json();
-                setMessages(messagesResponseJson._embedded.messages);
-                setTotalPages(messagesResponseJson.page.totalPages);
-            }
-            setIsLoadingMessages(false);
+  if (isLoadingMessages) {
+    return <SpinnerLoading />;
+  }
 
-        };
-        fetchUserMessages().catch((error: any) => {
-            setIsLoadingMessages(false);
-            setHttpError(error.message);
-        });
-        window.scrollTo(0, 0);
-    }, [authState, currentPage]);
-
-    if(isLoadingMessages) {
-        return <SpinnerLoading/>;
-    }
-
-    if(httpError) {
-        return <div className="container m-5">
-            <p>{httpError}</p>
-        </div>
-    }
-    const paginate = (pageNumber: number) => {
-        setCurrentPage(pageNumber);
-    }
-    return ();
-}
+  if (httpError) {
+    return (
+      <div className="container m-5">
+        <p>{httpError}</p>
+      </div>
+    );
+  }
+  const paginate = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+  return (
+    <div className="mt-3">
+      {messages.length > 0 ? (
+        <>
+          <h5>Pending Q/A: </h5>
+          {messages.map((message) => (
+            <>
+              <AdminMessage message={message} key={message.id} />
+            </>
+          ))}
+        </>
+      ) : (
+        <>
+          <h5>No pending Q/A</h5>
+        </>
+      )}
+      {totalPages > 1 && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          paginate={paginate}
+        />
+      )}
+    </div>
+  );
+};
