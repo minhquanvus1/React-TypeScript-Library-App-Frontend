b1ccae0ff1405471298d2c4cc29c250f56cd7a0b
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-21 22:02:50 +0700

    configure redirectURI in oktaConfig.ts to supoort https

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oabcn50qhlh7szMi5d7',
     issuer: 'https://dev-96351608.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:3000/login/callback',
+    redirectUri: 'https://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true
