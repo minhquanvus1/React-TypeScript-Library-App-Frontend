fc6ba27cdb7f1d3ce853ab55d3dbc4f24b1ada38
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-22 20:40:09 +0700

    configure index.html and index.tsx to support Stripe

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { App } from "./App";
 import { BrowserRouter } from "react-router-dom";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(
+  "pk_test_51NpsD9DHIQxG8E4lE74TOjNRhD52ODDWBPdRYaZYWt83cPOemyoqqNiLCr8p9zYZ7VaUffh3EmeJVRTLk0gzRiyM00cPmpbUlr"
+);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <BrowserRouter>
-    <App />
+    <Elements stripe={stripePromise}>
+      <App />
+    </Elements>
   </BrowserRouter>
 );
 
