e30e2919b111a5a027f4afc8ace31274afb1146d
TRẦN NGUYỄN MINH QUÂN <17640@student.vgu.edu.vn>
2023-09-23 20:54:22 +0700

    add SecureRoute for /fees, so that only authenticated user can go to this route

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { ReviewListPage } from "./layouts/BookCheckoutPage/ReviewListPage/Review
 import { ShelfPage } from "./layouts/ShelfPage/ShelfPage";
 import { MessagesPage } from "./layouts/MessagesPage/MessagesPage";
 import { ManageLibraryPage } from "./layouts/ManageLibraryPage/ManageLibraryPage";
+import { PaymentPage } from "./layouts/PaymentPage/PaymentPage";
 
 const oktaAuth = new OktaAuth(oktaConfig);
 export const App = () => {
@@ -64,6 +65,9 @@ export const App = () => {
             </SecureRoute>
             <SecureRoute path="/admin">
               <ManageLibraryPage />
+            </SecureRoute>
+            <SecureRoute path="/fees">
+              <PaymentPage />
             </SecureRoute>
           </Switch>
         </div>
